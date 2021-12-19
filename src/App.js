539044c1351cb3288240b2d5309a8632cd410495
Ramki Marichamy <ramkipandian@ramkis-MacBook-Pro.local>
2021-12-19 11:57:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import './Todo.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [value,setvalue] = useState([]);
+  
+  const [id,setid] = useState([]);
+  
+  
+  
+  
+  let handleClick = () => {
+   
+      setvalue([...value, `Task ${value.length + 1}`])
+  }
+
+  let removeClick = () => {
+    setid([...id, `id ${id.length}`])
+  
+      document.getElementById(`id.length`).remove();
+  }
+  return ( <div className="container">
+    <p>TODO LIST</p>
+  <div className="row">
+      <div className="col-md-12">
+          <div className="card card-white">
+            
+              <div className ='buttonele'>
+                    <button className= 'new btn btn-primary'  onClick={handleClick}>ADD</button>
+                      <br/>
+                    
+                      </div>
+                    <ul>
+                      {
+                     
+                       (value.map((item) => <li id = 'id.length'>{item}
+                        <br/> <span>Eat, Sleep, Repeat</span> <span className= 'del btn btn-danger' onClick={removeClick}>DELETE</span>
+                      </li>))
+                      
+
+                    }
+                      </ul>  
+                     
+                      </div>
+                
+                </div>
+                </div>
+                </div>
+                
+        );
 }
 
 export default App;
